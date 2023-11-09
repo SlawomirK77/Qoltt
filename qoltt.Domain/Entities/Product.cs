2b199014a694c5413af06b11353aecd768afd94f
Slawek <januszbak34@gmail.com>
2023-11-09 19:24:30 +0100

    added base entities

---
+++ qoltt.Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿namespace qoltt.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public ProductCategory Category { get; set; }
+        public List<string> Links { get; set; } = new();
+        public string? Picture { get; set; }
+        public decimal Rating { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid? CreatedById { get; set; }
+    }
+
+    public enum ProductCategory
+    {
+        Software,
+        Electronics,
+        Fashion,
+        HomeAndGarden,
+        Supermarket,
+        Kids,
+        Beauty,
+        Health,
+        CultureAndEntertainment,
+        SportsAndTravel,
+        Automotive,
+        CollectionsAndArt,
+        BuisnessAndServices
+    }
+}
---
+++ qoltt.Domain/Entities/Tip.cs
@@ -0,0 +1,31 @@
+﻿namespace qoltt.Domain.Entities
+{
+    public class Tip
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public ProductCategory Category { get; set; }
+        public List<string> Links { get; set; } = default!;
+        public decimal Rating { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid? CreatedById { get; set; }
+    }
+
+    public enum ProductCategory
+    {
+        Software,
+        Electronics,
+        Fashion,
+        HomeAndGarden,
+        Supermarket,
+        Kids,
+        Beauty,
+        Health,
+        CultureAndEntertainment,
+        SportsAndTravel,
+        Automotive,
+        CollectionsAndArt,
+        BuisnessAndServices
+    }
+}
